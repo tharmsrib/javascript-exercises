ef08cf3230456d0f0b0bf13297564c5ceceef7af
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-17 15:51:36 -0700

    finished ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(inputString,numRepeat) {
+    if (numRepeat < 0) return "ERROR";
+    let outputString = "";
+    
+    for(let i=0;i<numRepeat;i++){
+        outputString += inputString;
+    }
+    return outputString
 };
 
 // Do not edit below this line
