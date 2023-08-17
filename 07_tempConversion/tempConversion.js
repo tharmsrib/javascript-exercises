7f364de2ae5ecb4d32972ddc93964d9d156cc6d5
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-17 16:10:07 -0700

    finished ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let output = (temperature - 32)*5/9;
+  return +output.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let output = (temperature* 9/5) + 32;
+  return +output.toFixed(1)
 };
 
 // Do not edit below this line
