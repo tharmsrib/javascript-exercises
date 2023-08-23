e426d726b961852f9fad10221f6d2083b331234c
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-23 11:57:14 -0700

    finished ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    //convert to lower case
+    let string = input.toLowerCase();
+    //remove spaces
+    string = string.replace(/[^a-zA-Z0-9]/g,'');
+    reverseString = reverse(string);
+    
 
+    console.log(reverseString);
+    //cmp 
+    if(string == reverseString){
+        return true;
+    }else{
+        return false;
+    }
 };
+function reverse(s){
+    return s.split('').reverse().join('');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 09_palindromes/palindromes.spec.js
+++ 09_palindromes/palindromes.spec.js
@@ -10,7 +10,7 @@ describe('palindromes', () => {
   test.skip('works with upper-case letters ', () => {
     expect(palindromes('Racecar!')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with multiple words', () => {
     expect(palindromes('A car, a man, a maraca.')).toBe(true);
   });
   test.skip('works with multiple words', () => {
@@ -19,7 +19,7 @@ describe('palindromes', () => {
   test.skip('doesn\'t just always return true', () => {
     expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
   });
-  test.skip('works with numbers in a string', () => {
+  test('works with numbers in a string', () => {
     expect(palindromes('rac3e3car')).toBe(true);
   });
   test.skip('works with unevenly spaced numbers in a string', () => {
