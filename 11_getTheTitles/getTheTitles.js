1da2a122575742eb459eaf2322fc01e36e37b225
tharmsri007 <57075136+tharmsrib@users.noreply.github.com>
2023-08-23 15:06:56 -0700

    finished ex11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayObj) {
+    // Your job is to write a function that takes the array and returns an array of titles:
+    const bookNames = arrayObj.map(book => {
+        // console.log(book.title);
+        return book.title;
+    });
+    // console.log(bookNames);
+    return bookNames;
 };
 
 // Do not edit below this line
